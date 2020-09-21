1231cabd1e50e9a6c444fc02c9b0bfd33607eafe
fieldstyler <tdfields93@gmail.com>
2020-09-21 09:33:54 -0600

    Completed User Story 1

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Flight Index Page' do
+  before :each do
+    @airline = Airline.create(name: "First Airline")
+    @flight_1 = @airline.flights.create(number: 1234)
+    @flight_2 = @airline.flights.create(number: 2345)
+    @flight_3 = @airline.flights.create(number: 3456)
+    @passenger_1 = @flight_1.passengers.create(name: "Tyler", age: 10)
+    @passenger_2 = @flight_1.passengers.create(name: "David", age: 20)
+    @passenger_3 = @flight_1.passengers.create(name: "Alex", age: 30)
+    @passenger_4 = @flight_2.passengers.create(name: "Dan", age: 40)
+    @passenger_5 = @flight_2.passengers.create(name: "Saryn", age: 50)
+    @passenger_6 = @flight_3.passengers.create(name: "Tim", age: 60)
+    @passenger_7 = @flight_3.passengers.create(name: "Brian", age: 70)
+    @passenger_8 = @flight_3.passengers.create(name: "Dani", age: 80)
+    @flights = Flight.all
+    visit flights_path
+  end
+
+  describe 'user story 1' do
+    it 'can display a list of all flight numbers, and under each flight number are the names of all of that flights passengers' do
+      @flights.each do |flight|
+        expect(page).to have_content(flight.number)
+        flight.passengers.each do |passenger|
+          expect(page).to have_content(passenger.name)
+        end
+      end
+    end
+  end
+end
